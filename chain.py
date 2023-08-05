9c898725223bf0106721f1736e761ddcfa2295bc
git-frnksr <frnksr@gmail.com>
2023-08-05 14:53:29 +0200

    adding load qa chain with sources

--- chain.py
+++ chain.py
@@ -5,8 +5,10 @@ from langchain.callbacks.tracers import LangChainTracer
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chains.chat_vector_db.prompts import (CONDENSE_QUESTION_PROMPT,
                                                      QA_PROMPT)
+from langchain.prompts.prompt import PromptTemplate
 from langchain.chains.llm import LLMChain
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.vectorstores.base import VectorStore
@@ -44,12 +46,14 @@ def get_chain(
         temperature=0,
         verbose=True,
         callback_manager=question_manager,
+        max_tokens=2000
     )
     streaming_llm = OpenAI(
         streaming=True,
         callback_manager=stream_manager,
         verbose=True,
         temperature=0,
+        max_tokens=2000
     )
 
     question_generator = LLMChain(
@@ -58,6 +62,26 @@ def get_chain(
     doc_chain = load_qa_chain(
         streaming_llm, chain_type="stuff", prompt=QA_PROMPT, callback_manager=manager
     )
+    
+    template = """Given the following extracted parts of a long document and a question, create a final answer with references ("SOURCES").
+    If you don't know the answer, just say that you don't know. Don't try to make up an answer.
+    ALWAYS return a "SOURCES" part in your answer. Try to add the page number of the document context. Always answer in German.
+
+    QUESTION: {question}
+    =========
+    {summaries}
+    =========
+    {chat_history}
+    =========
+    FINAL ANSWER:"""
+    
+    SOURCES_QA_PROMPT = PromptTemplate(
+        template=template, input_variables=["summaries", "question", "chat_history"]
+    )
+
+    doc_chain_sources = load_qa_with_sources_chain(
+        llm=streaming_llm, chain_type="stuff", prompt=SOURCES_QA_PROMPT, callback_manager=manager
+    )
 
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True, output_key='answer')
@@ -65,7 +89,7 @@ def get_chain(
     qa = ConversationalRetrievalChain(
         retriever=vectorstore.as_retriever(),
         memory=memory,
-        combine_docs_chain=doc_chain,
+        combine_docs_chain=doc_chain_sources,
         question_generator=question_generator,
         callback_manager=manager,
         return_source_documents=True
